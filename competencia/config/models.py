55b868b54315c49876e5b620b3d09ef5210830bf
Elessar93 <rcardenas93@gmail.com>
2019-01-02 18:17:48 -0600

    init

---
+++ competencia/config/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+
+class Catalogo(models.Model):
+    nombre = models.CharField(max_length=64)
+    estatus = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        abstract = True
+
+
+class Aparato(Catalogo):
+    class Meta:
+        managed = True
+        db_table = 'aparato'
+
+
+class Calificacion(models.Model):
+    aparato = models.ForeignKey('Aparato', models.DO_NOTHING)
+    calificacion = models.FloatField()
+    competidor = models.ForeignKey('Competidor', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'calificacion'
+
+
+class Competencia(models.Model):
+    fecha = models.DateField()
+    estatus = models.BooleanField()
+
+    class Meta:
+        managed = True
+        db_table = 'competencia'
+
+
+class Competidor(models.Model):
+    nombre = models.CharField(max_length=128)
+    apellidos = models.CharField(max_length=128)
+    nivel = models.ForeignKey('Nivel', models.DO_NOTHING)
+    gimnasio = models.ForeignKey('Gimnasio', models.DO_NOTHING)
+    edad = models.IntegerField()
+    competencia = models.ForeignKey('Competencia', models.DO_NOTHING)
+    grupo = models.ForeignKey('Grupo', models.DO_NOTHING)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'competidor'
+
+
+class Gimnasio(Catalogo):
+    class Meta:
+        managed = True
+        db_table = 'gimnasio'
+
+
+class Grupo(models.Model):
+    competencia = models.ForeignKey('Competencia', models.DO_NOTHING)
+    aparato = models.ForeignKey('Aparato', models.DO_NOTHING, blank=True, null=True)
+
+
+class Nivel(Catalogo):
+    class Meta:
+        managed = True
+        db_table = 'nivel'
